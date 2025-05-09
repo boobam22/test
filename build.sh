d6726b63caea18e325f0eed88b6db61a877cde62
nia <mangrove0720@outlook.com>
2025-05-09 17:27:06 +0800

    workflow

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-docker build -t test .
+dirs=$(find src -name __pycache__)
+if [ -n "$dirs" ]; then
+    rm -r $dirs
+fi
+
+mkdir -p dist
+python3 -m zipapp src -p '/bin/env python3' -o dist/github-api.pyz
